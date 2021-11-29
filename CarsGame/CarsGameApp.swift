5a7458e1d7d0790cdf75281daad73236d6e90602
Leo Lu <llu@lcs.on.ca>
2021-11-29 14:25:53 -0500

    Initial Commit

---
+++ CarsGame/CarsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsGameApp.swift
+//  CarsGame
+//
+//  Created by Leo Lu on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct CarsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
