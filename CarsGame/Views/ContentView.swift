01de7b66f6c77906bfefe4c5b8e8ad8b9b5acf08
Leo Lu <llu@lcs.on.ca>
2021-11-30 19:42:20 -0500

    created homepage for the car game

--- CarsGame/Views/ContentView.swift
+++ CarsGame/Views/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        NavigationView{
+            VStack {
+             Text("car photo game")
+                    .font(.title)
+               
+                Text("You have 20 seconds to complete each question")
+                Image("imagecontent")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 500, height: 500)
+                
+                
+                
+                NavigationLink(destination: {Text("Placeholder")}){
+                    Text("start game")
+                        .font(.largeTitle)
+                }
+                .padding()
+                Text("good luck in the game")
+
+            
+                Spacer()
+                
+        }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
